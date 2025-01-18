9adc1ca0a3af41c5bd703293bdb1b5b0c2a5530f
artur <fowlart1988@gmail.com>
2025-01-18 13:16:01 +0200

    added identification of keyring backend

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 from telethon import TelegramClient
 import keyring as kr
 
+def define_keyring_backend():
+    key_ring = kr.get_keyring()
+    key_ring_name = str(key_ring).split(" ")[0]
+    print(f"Setting keyring with the name: {key_ring_name}")
+    kr.core.set_keyring(kr.core.load_keyring(key_ring_name))
+    pass
+
 def get_tg_client() -> TelegramClient:
+    define_keyring_backend()
     api_id = kr.get_password("telegram","api_id")
     api_hash = kr.get_password("telegram","api_hash")
     return TelegramClient("init_session", int(api_id), api_hash)
@@ -15,3 +23,6 @@ async def send_msg_to_myself(client: TelegramClient, msg: str):
 async def print_dialogs(client: TelegramClient):
     async for dialog in client.iter_dialogs():
         print(dialog.name, "has id",dialog.id)
+
+if __name__=="__main__":
+    define_keyring_backend()
