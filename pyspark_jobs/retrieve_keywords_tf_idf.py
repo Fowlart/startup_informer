59235e3878b149acf2a9838fe9261ef8dcb2ce3e
artur <fowlart1988@gmail.com>
2025-03-12 14:13:16 +0200

    added term dictionary resolution for the tf/idf processing

--- pyspark_jobs/retrieve_keywords_tf_idf.py
+++ pyspark_jobs/retrieve_keywords_tf_idf.py
@@ -1,10 +1,7 @@
 from pyspark.sql import SparkSession, Column
-from pyspark.sql.functions import udf, col, filter, regexp_extract, length, collect_set, flatten, explode, collect_list, \
-    size, array_distinct
-from pyspark.sql.window import Window
+from pyspark.sql.functions import udf, col, filter, regexp_extract, length, explode, lit, array, collect_set
 from pyspark.sql.types import ArrayType,StringType
-from pyspark.ml.feature import HashingTF, IDF
-
+from pyspark.ml.feature import HashingTF, IDF, CountVectorizer
 
 from delta import *
 from azure_ai_utils import analyze_text
@@ -26,6 +23,26 @@ def extract_tokens_udf(input_text: str) -> list[str]:
     )
 
     return [token_obj["token"] for token_obj in tokens_obj_list if "token" in token_obj]
+
+# UDF to extract keywords
+@udf(returnType=ArrayType(StringType()))
+def extract_keywords_udf(tfidf_vector, vocabulary: list[str]):
+
+    if tfidf_vector is None:
+        return []
+
+    word_tfidf_pairs = []
+
+    for i, tfidf_score in enumerate(tfidf_vector):
+        # configure this
+        if tfidf_score > 2:
+            word_tfidf_pairs.append((tfidf_score, vocabulary[i]))
+
+    sorted_pairs = sorted(word_tfidf_pairs, key=lambda x: x[0], reverse=True)
+
+    top_keywords = [pair[1] for pair in sorted_pairs[:10]]
+
+    return top_keywords
 
 def _filter_words_with_digits(x: col)->Column:
     return regexp_extract(x, r"\d", 0) == ""
@@ -53,45 +70,28 @@ if __name__ == "__main__":
           .filter(col("user_id") == "553068238")
 
            #todo: limit will speed up the process, remove after testing
-           .limit(30)
+           #.limit(30)
 
           .withColumn("tokens",extract_tokens_udf(col("message_text")))
           .withColumn("tokens", filter(col("tokens"), _words_length_filter))
           .withColumn("tokens",filter(col("tokens"),_filter_words_with_digits))
           )
 
-    window_definition = Window.rowsBetween(start=Window.unboundedPreceding,end=0)
-
-    dictionary_df = (df
-                     .withColumn("dict",array_distinct(flatten(collect_list(col("tokens")).over(window_definition))))
-                     .withColumn("dict_size",size(col("dict")))
-                     .orderBy(col("dict_size").desc())
-                     .limit(1)
-                     )
-
-    dictionary = dictionary_df.take(1)[0]["dict"]
-
-    print(f"The dictionary: {dictionary}")
-    print(f"The dictionary length: {len(dictionary)}")
-
     # spark tf/idf
-    tf = (
-        HashingTF()
-        .setInputCol("tokens")
-        .setOutputCol("tf_out")
-        .setNumFeatures(len(dictionary)))
+    cv = CountVectorizer(inputCol="tokens", outputCol="tf_out")
+    cv_model = cv.fit(df)
+    df_tf = cv_model.transform(df)
+
 
     idf = (
         IDF()
         .setInputCol("tf_out")
         .setOutputCol("idf_out")
-        .setMinDocFreq(3))
-
-    tf_transformed = tf.transform(df)
+        .setMinDocFreq(2))
 
     df_with_idf_info = (idf
-     .fit(tf_transformed)
-     .transform(tf_transformed)
+     .fit(df_tf)
+     .transform(df_tf)
      .select("user_id",
              "message_date",
              "message_text",
@@ -99,11 +99,22 @@ if __name__ == "__main__":
              "idf_out"
              ))
 
-    # write an intermediate steps to the disk for analysis
-    dictionary_df.write.json(path="./../key_words_extraction/debug_dictionary/", mode="overwrite")
+    # Apply the UDF
+    dictionary = cv_model.vocabulary
+    dict_df = (spark.sparkContext
+               .parallelize(dictionary)
+               .map(lambda x:(x,)).toDF(["terms"])
+               .groupBy()
+               .agg(collect_set("terms").alias("terms_array"))
+               .select("terms_array"))
 
+    dict_df.show()
+
+    ddf_with_keywords = dict_df.crossJoin(df_with_idf_info).withColumn("keywords", extract_keywords_udf(col("idf_out"), col("terms_array")))
+
+    # write an intermediate steps to the disk for debug and analysis
+    ddf_with_keywords.select(["user_id", "message_date","tokens","keywords"]).write.json(path="./../key_words_extraction/result/", mode="overwrite")
     df.write.json(path="./../key_words_extraction/debug_key_words_step_1/", mode="overwrite")
-
-    (df_with_idf_info.write.json(path="./../key_words_extraction/debug_key_words_step_2/", mode="overwrite"))
+    df_with_idf_info.write.json(path="./../key_words_extraction/debug_key_words_step_2/", mode="overwrite")
 
     spark.stop()
